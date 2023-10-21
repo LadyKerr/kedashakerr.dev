cd3da4b974751c582d4695ae4b1109c10f3e5579
Kedasha <ladykerr@github.com>
2023-10-21 12:47:21 -0500

    testing

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'Kedasha Kerr Devrel portfolio',
+    description: 'All the content she has produced for devrel',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
